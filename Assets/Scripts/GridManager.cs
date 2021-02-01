a9564c65093dbf16b1dd6d4dcb2a4b8668df4ef4
xFluke <mikoman1124@gmail.com>
2021-02-01 12:06:52 -0500

    Made Grid Generation a bit more customizable

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GridManager : MonoBehaviour
 {
@@ -12,6 +13,9 @@ public class GridManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        float tileSize = GetComponent<GridLayoutGroup>().cellSize.x;
+        GetComponent<RectTransform>().sizeDelta = new Vector2(sizeX * tileSize, sizeY * tileSize);
+
         for (int x = 0; x < sizeX; x++) {
             for (int y = 0; y < sizeY; y++) {
                 Instantiate(tilePrefab, this.transform);
